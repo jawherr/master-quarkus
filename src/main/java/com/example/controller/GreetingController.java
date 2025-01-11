9ab925b14ccaebae847993bf6f2b0faea8dce108
Jawher <kallel.jawher@gmail.com>
2025-01-11 17:11:46 +0100

    Update GreetingController.java

--- src/main/java/com/example/controller/GreetingController.java
+++ src/main/java/com/example/controller/GreetingController.java
@@ -11,6 +11,6 @@ public class GreetingController {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello, Quarkus!";
+        return "Hello, Quarkus!!";
     }
 }
